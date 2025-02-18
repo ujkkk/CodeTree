3621da82584007a66c6a28cba3de361c3bd92fab
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-18 21:24:14 +0900

    Title: 계단 오르기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250218/계단 오르기/climbing-stairs.java
+++ 250218/계단 오르기/climbing-stairs.java
@@ -6,11 +6,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int [] dp = new int[N+5];
+        int [] dp = new int[N+10];
         dp[2] = dp[3] = 1;
         dp[4] = 2;
         for(int i=5; i<=N; i++){
-            dp[i] = (dp[i-2] + dp[i-3])%10007;
+            dp[i] = (dp[i-2] + dp[i-3])%10_007;
         }
 
         System.out.println(dp[N]);
