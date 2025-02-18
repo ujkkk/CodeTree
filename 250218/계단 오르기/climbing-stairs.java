97157572e406e93056e6d78f27da1b9347654881
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-18 21:25:28 +0900

    Title: 계단 오르기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250218/계단 오르기/climbing-stairs.java
+++ 250218/계단 오르기/climbing-stairs.java
@@ -8,7 +8,7 @@ public class Main {
 
         int [] dp = new int[N+10];
         dp[2] = dp[3] = 1;
-        dp[4] = 2;
+        dp[4] = 1;
         for(int i=5; i<=N; i++){
             dp[i] = (dp[i-2] + dp[i-3])%10_007;
         }
