7215072e76df5ecdead31a83ace0d4f17ef0bc13
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-18 23:13:58 +0900

    Title: 사각형 채우기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250218/사각형 채우기/rectangle-fill.java
+++ 250218/사각형 채우기/rectangle-fill.java
@@ -10,7 +10,7 @@ public class Main {
         dp[2] = 2;
 
         for(int i=3; i<=N; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
 
         System.out.println(dp[N]);
