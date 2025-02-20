b8e6a49cc27d0637cd375470515d5da50d8613c1
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-20 13:29:43 +0900

    Title: 2차원 최대 증가 수열, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250220/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 250220/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -22,7 +22,7 @@ public class Main {
         int [][] dp = new int[N][M];
         for(int i = 0; i < N; i++)
             for(int j = 0; j < M; j++)
-                dp[i][j] = -1;
+                dp[i][j] = 0;
 
         dp[0][0] = 1;
 
