dffbb3c7a924b4f4f0c1566f649f880e957570b0
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-20 14:38:13 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250220/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 250220/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -4,47 +4,74 @@ import java.io.*;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    public static int [][] dp;
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
         ArrayList<Line> lines = new ArrayList();
 
         StringTokenizer st;
+        int max = 0;
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
+            max = Math.max(end, max);
 
             lines.add(new Line(start, end));
         }
 
         Collections.sort(lines);
 
-        int [][] dp = new int[1001][2];
+        dp = new int[max+1][2];
+    
     
         for(Line line : lines){
-            for(int i=0; i<line.start; i++){
-                // 이전 줄의 끝점이 현재 줄의 start보다 작아야 함
-                if(dp[i][1] >= line.start){
-                    continue;
-                }
-                for(int j=line.start; j<=line.end; j++){
-                    if(dp[j][0] < dp[i][0] +1){
-                        dp[j][0] = Math.max(dp[j][0], dp[i][0] +1);
-                        dp[j][1] = line.end;
+            for(int i=line.start; i<=line.end; i++){
+                for(int j=0; j<line.start; j++){
+                    // 이전 줄의 끝점이 현재 줄의 start보다 작아야 함
+                    if(dp[j][1] >= line.start){
+                        continue;
                     }
+                    if(dp[i][0] < dp[j][0] +1){
+                        dp[i][0] = Math.max(dp[i][0], dp[j][0] +1);
+                        dp[i][1] = line
                 }
             }
+            //print();
         }
 
         int ans = 0;
-        for(int i=0; i<100; i++){
-            //System.out.print(dp[i][0] +" ");
+        for(int i=0; i<=max; i++){
             ans = Math.max(ans, dp[i][0]);
         }
-        //System.out.println();
         System.out.println(ans);
+
+
+.end;
+                    }
+                    //dp[i][1] = Math.min(dp[i][1], dp[j][1]);
+                }
+            }
+            //print();
+        }
+
+        int ans = 0;
+        for(int i=0; i<=max; i++){
+            ans = Math.max(ans, dp[i][0]);
+        }
+        System.out.println(ans);
+    }
+
+    public static void print(){
+        for(int i=0; i<10; i++){
+            System.out.print(dp[i][0] +" ");
+        }
+        System.out.println();
+        for(int i=0; i<10; i++){
+            System.out.print(dp[i][1] +" ");
+        }
+        System.out.println();
     }
 }
 
