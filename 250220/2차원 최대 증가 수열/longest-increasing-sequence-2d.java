c573a40864a6eba526c47f214884a62d917da4e0
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-20 13:07:43 +0900

    Title: 2차원 최대 증가 수열, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250220/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        // O(n^2) 최대 65천만
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [][] map = new int[N][M];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int [][] dp = new int[N][M];
+        dp[0][0] = 1;
+
+        for(int i=1; i<N; i++){
+            for(int j=1; j<M; j++){
+
+                // (i, j) 로 올수 있는 칸은 (0~i-1, 0~j-1)킨 들
+                for(int a=0; a<=i-1; a++){
+                    for(int b=0; b<=j-1; b++){
+                        if(map[i][j] <= map[a][b]){
+                            continue;
+                        }
+                        dp[i][j] = Math.max(dp[i][j], dp[a][b] +1);
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        System.out.println(ans);
+
+        
+    }
+}
