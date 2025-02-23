6ce9c00095d37834a37668b5e619e912927e994f
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-23 15:53:23 +0900

    Title: 강력한 폭발, Time: 968ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250223/강력한 폭발/strong-explosion.java
@@ -0,0 +1,131 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int N;
+    public static int [][] check;
+    public static ArrayList<Point> bombs;
+
+    public static int ans = 0;
+
+    public static int getPoint(int [][] check){
+        int count = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(check[i][j] > 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void checkBombs(int [][] check, int r, int c, int kind){
+        //System.out.println(String.format("%d %d, %d 종류 체크", r, c, kind));
+        int [][] dr = { {-1, -2, 1, 2}, {-1, 0, 1, 0}, {-1, -1, 1, 1} };
+        int [][] dc = { {0, 0, 0, 0,}, {0, 1, 0, -1}, {-1, 1, 1, -1}};
+
+        check[r][c]++;
+        for(int i=0; i<dr[kind].length; i++){
+            int nr = r + dr[kind][i];
+            int nc = c + dc[kind][i];
+
+            if(nr <0 || nr >=N || nc <0 || nc >=N)
+                continue;
+            
+            check[nr][nc]++;
+        }
+        return;
+    }
+
+    public static void removeBombs(int [][] check, int r, int c, int kind){
+        //System.out.println(String.format("%d %d, %d 종류 삭제", r, c, kind));
+        int [][] dr = { {-1, -2, 1, 2}, {-1, 0, 1, 0}, {-1, -1, 1, 1} };
+        int [][] dc = { {0, 0, 0, 0,}, {0, 1, 0, -1}, {-1, 1, 1, -1}};
+
+        check[r][c]--;
+        if(check[r][c] < 0){
+            check[r][c] = 0;
+        }
+        for(int i=0; i<dr[kind].length; i++){
+            int nr = r + dr[kind][i];
+            int nc = c + dc[kind][i];
+
+            if(nr <0 || nr >=N || nc <0 || nc >=N)
+                continue;
+            
+            check[nr][nc]--;
+            if(check[nr][nc] < 0){
+                check[nr][nc] = 0;
+            }
+        }
+        return;
+    }
+
+
+    public static void select(int depth, int [][] check){
+        if(depth == bombs.size()){
+            int count = getPoint(check);
+            // print(check);
+            // System.out.println("###########");
+            // // if(count > ans){
+            // //     print(check);
+            // //     System.out.println("###########");
+            // // }
+            ans = Math.max(count, ans);
+            return;
+        }
+
+        for(int i=depth; i<bombs.size(); i++){
+            int r = bombs.get(i).r;
+            int c = bombs.get(i).c;
+
+            for(int j=0; j<3; j++){
+                checkBombs(check, r, c, j);
+                select(depth +1, check);
+                removeBombs(check, r, c, j);
+            }
+        }
+    }
+
+    public static void print(int [][] check){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                System.out.print(check[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        check = new int[N][N];
+        bombs = new ArrayList();
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                int n = Integer.parseInt(st.nextToken());
+                if(n == 1){
+                    bombs.add(new Point(i, j));
+                }
+            }
+        }
+
+        select(0, check);
+        System.out.println(ans);
+
+    }
+}
+
+class Point{
+    int r;
+    int c;
+
+    public Point(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
+}
