2d8139b4295e2d9c0395e602ba54062e47a1fece
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-03-04 10:46:51 +0900

    Title: 아름다운 수, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 250304/아름다운 수/beautiful-number.java
+++ 250304/아름다운 수/beautiful-number.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
+/*
+어려웠던 점 : count를 static 변수를 사용안하고 누적해서 넘겨주는 것
+*/
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = magicNum(0, new int[n], n);
+        int sum = countMagicNums(0, new int[n], n);
 
         System.out.println(sum);
     }
 
-    public static int magicNum(int depth, int [] nums, int N){
+    public static int countMagicNums(int depth, int [] nums, int N){
         if(depth == N){
             // 아름다운 수인지 체크
-            if(isMagicNum(nums)){
-                // System.out.println("매직넘");
-                return 1;
-            }
-            return 0;
+            return isMagicNum(nums)? 1 : 0;
         }
 
         int sum = 0;
         for(int i=1; i<=4; i++){
             nums[depth] = i;
-            sum += magicNum(depth +1, nums, N);
+            sum += countMagicNums(depth +1, nums, N);
         }
         return sum;
     }
@@ -31,21 +30,11 @@ public class Main {
         int size = nums.length;
         int idx = 0;
 
-        // for(int n : nums){
-        //     System.out.print(n +" ");
-        // }
-        // System.out.println();
 
         while(idx < size){
             int cur = nums[idx];
             for(int i=idx; i<idx+cur; i++){
-                if(i>= size){
-                    // System.out.println("사이즈 초과");
-                    return false;
-                    
-                }
-                if(nums[i] != cur){
-                    // System.out.println(i +"번 째에서 같지 않음");
+                if(i>= size || nums[i] != cur){
                     return false;
                 }
             }
